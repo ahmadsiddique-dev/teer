c4abe9931456a76450ccabe1d9255637445be738
Ahmad Siddique <asshikrani66@gmail.com>
2026-04-05 21:24:11 +0500

    minor bugs becuse of path refrence and also change in readme status

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
+import { Link } from "react-router";
 import { Button } from "./components/ui/8bit/button";
 
 const App = () => {
     return (
         <div className="w-full flex justify-center text-white pt-4">
             <p className="retro">Please go to chat page...</p>
-            <Button className="self-center"><a href="/chat">GO</a></Button>
+            <Button className="self-center"><Link to={"/chat"}>GO</Link></Button>
         </div>
     );
 };
---
+++ readme.md
@@ -0,0 +1,5 @@
+# 🚧 Still in Development 🚧
+
+> Teer is a a chat app where a user can go and create an account without verifying. He can choose a unique username which will be used to target a specific person to chat. 
+
+> This chat will be anonymous between both and will be destroyed after 7 hours. One more thing that this chat is in 8-bit design which makes it supper cool.
