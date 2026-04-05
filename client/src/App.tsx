a6d32ae2c19cc37eebbf7fa55f6ea055a7b723fd
Ahmad Siddique <asshikrani66@gmail.com>
2026-04-05 21:00:51 +0500

    a little change in message...

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,10 @@
+import { Button } from "./components/ui/8bit/button";
 
 const App = () => {
     return (
-        <div>
-            Hey ther here is app page
+        <div className="w-full flex justify-center text-white pt-4">
+            <p className="retro">Please go to chat page...</p>
+            <Button className="self-center"><a href="/chat">GO</a></Button>
         </div>
     );
 };
